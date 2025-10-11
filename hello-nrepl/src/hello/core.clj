91ef6630fced577283e55b6f21e086555a7f291d
uridongsu <uridongsu@gmail.com>
2025-10-11 10:43:09 +0900

    refactor: nrepl

--- hello-nrepl/src/hello/core.clj
+++ hello-nrepl/src/hello/core.clj
@@ -3,12 +3,14 @@
 (defn hello [{:keys [name]}]
   (prn :Hello name))
 
-(defn -main
-  [& [name]]
-  (hello {:name name})
-  ;; run nrepl
+(defn run-nrepl []
   (try
     (when-let [nrepl-main (requiring-resolve 'nrepl.cmdline/-main)]
       (nrepl-main "--port" "8888" "--middleware" "[cider.nrepl/cider-middleware]"))
     (catch Exception e
       (prn :error (.getMessage e)))))
+
+(defn -main
+  [& [name]]
+  (hello {:name name})
+  (run-nrepl))
